dcd2042a83bfa144d1aba6f59377c79dd9c9692e
abdallahEssam49 <76582213+abdallahEssam49@users.noreply.github.com>
2023-01-09 06:52:15 +0200

    Update Number of Islands.cpp

--- Graph/Depth-First Search/Number of Islands.cpp
+++ Graph/Depth-First Search/Number of Islands.cpp
@@ -29,3 +29,37 @@ public:
         return islands;
     }
 };
+
+
+// Another Solution using memoization
+
+class Solution {
+int solve(vector<vector<char>>& grid,int i,int j,vector<vector<int>> &dp){
+    int m=grid.size();
+    int n=grid[0].size();
+    if(i<0 || j<0 || i>=m || j>=n || grid[i][j]=='0') return 0;
+    
+    if(dp[i][j]!= -1) return dp[i][j];
+    grid[i][j]='0';
+  
+  return dp[i][j] = 1+solve(grid,i+1,j,dp)+solve(grid,i-1,j,dp)+solve(grid,i,j-1,dp)+solve(grid,i,j+1,dp);
+
+}
+public:
+    int numIslands(vector<vector<char>>& grid){
+        ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+        int m=grid.size();
+        int n=grid[0].size(),len=0;
+       vector<vector<int>> dp(m,vector<int>(n,-1));
+        for(int i=0;i<m;i++){
+         for(int j=0;j<n;j++){
+             if(grid[i][j]=='1'){
+               if(solve(grid,i,j,dp)>=1)
+                len++;  
+             }
+            
+         }  
+        }
+        return len;
+    }
+};
