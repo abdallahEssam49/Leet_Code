1407489ec3d0c1be2f02ce42ef2eedf1319fd187
abdallahEssam49 <76582213+abdallahEssam49@users.noreply.github.com>
2023-01-09 07:04:26 +0200

    Update 62. Unique Paths.cpp

--- DP_Problems/Medium/62. Unique Paths.cpp
+++ DP_Problems/Medium/62. Unique Paths.cpp
@@ -6,6 +6,7 @@ public:
     
     int solve(int n , int m , int row , int col , vector<vector<int>>&dp)
     {
+        ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
         if (row == n and col == m)return 1;
         if (row > n or col > m)return 0;
         
@@ -22,4 +23,23 @@ public:
             vector<vector<int>>dp(n+1 , vector<int>(m+1 ,-1));
             return solve(n , m , 1 ,1 , dp);
         }
+};
+
+
+// Another Solution using Tabulation
+https://www.geeksforgeeks.org/tabulation-vs-memoization/
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 1));
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+        
+    }
 };
