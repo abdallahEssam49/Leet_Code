5de0a32d61f866bb38d8fc4ec94c82b0ae201057
abdallahEssam49 <76582213+abdallahEssam49@users.noreply.github.com>
2023-01-15 09:06:34 +0200

    Update 3. Longest Substring Without Repeating Characters.cpp

--- Study Plan/Algorithm I/Day 6/3. Longest Substring Without Repeating Characters.cpp
+++ Study Plan/Algorithm I/Day 6/3. Longest Substring Without Repeating Characters.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
 	int lengthOfLongestSubstring(string s) 
 	{
-        	ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
-        	int len = s.length();
-		unordered_set<char> set;
+		ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+		int len = s.length();
+		unordered_map<char , int > map;
 		int i = 0 , j = 0 , ret = 0;
-    
+
 		while(j<len)
 		{
-			if(set.find(s[j]) == set.end()) 
+			if(!map[s[j]]) 
 			{
-				set.insert(s[j++]); 
+				map[s[j++]]++;
 				ret = max(ret, j-i); 
 			}
-			else set.erase(s[i++]); 
+			else map[s[i++]]--; 
 		}
-    
+
 		return ret;
 	}
 };
