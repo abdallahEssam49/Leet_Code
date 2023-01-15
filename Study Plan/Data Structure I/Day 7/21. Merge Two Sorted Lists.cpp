7b06ec29602793f8ea217d593ecacf436f61df8f
abdallahEssam49 <76582213+abdallahEssam49@users.noreply.github.com>
2023-01-15 20:23:08 +0200

    Create 21. Merge Two Sorted Lists.cpp

---
+++ Study Plan/Data Structure I/Day 7/21. Merge Two Sorted Lists.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2){
+
+        ListNode* res = new ListNode(0); // dymmy node
+        ListNode* temp = res;
+        while(l1 && l2){
+            if(l1->val < l2->val)
+            {
+                temp->next = l1;
+                l1 = l1->next;
+            }
+            else
+            {
+                temp->next = l2;
+                l2 = l2->next;
+            }
+            temp = temp->next;
+        }
+        if(l1) temp->next = l1;
+        if(l2) temp->next = l2;
+        
+        return res->next;
+            
+    }
+};
