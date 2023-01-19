a90eaf87fa8885035a3c8137763fb09812b6884d
abdallahEssam49 <76582213+abdallahEssam49@users.noreply.github.com>
2023-01-19 16:54:26 +0200

    Update 144. Binary Tree Preorder Traversal.cpp

--- Study Plan/Data Structure I/Day 10/144. Binary Tree Preorder Traversal.cpp
+++ Study Plan/Data Structure I/Day 10/144. Binary Tree Preorder Traversal.cpp
@@ -20,3 +20,32 @@ public:
         return ans;
     }
 };
+
+// Iterative Solution
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) 
+    {
+        vector<int> nodes;
+        stack<TreeNode*> temp;
+        while (root || !temp.empty()) 
+        {
+            if (root) 
+            {
+                nodes.push_back(root -> val);
+                if (root -> right)
+                {
+                    temp.push(root -> right); // to get the last right i reached and if it has sub tree or not 
+                }
+                root = root -> left;
+            }
+            else 
+            {
+                root = temp.top(); // when reached the most left node of left subtree
+                temp.pop();
+            }
+        }
+        return nodes;
+    }
+};
